5e5425cd65a062fe137bb920fd504287697f0486
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-13 21:46:23 +0900

    페치 조인 1 - 기본

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -22,28 +22,56 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            entityManager.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            entityManager.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-            member.setTeam(team);
-            entityManager.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            entityManager.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            entityManager.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            entityManager.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            entityManager.persist(member3);
 
             entityManager.flush();
             entityManager.clear();
 
-//            String query = "select m.username from Member m";
-//            String query = "select m.team.name from Member m";
-            String query = "select m.username from Team t join t.members m";
+/*
+            String query = "select m from Member m join fetch m.team";
 
-            Collection result = entityManager.createQuery(query, Collection.class).getResultList();
+            List<Member> result = entityManager.createQuery(query, Member.class).getResultList();
 
-            for (Object o : result) {
-                System.out.println("o = " + o);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+            }
+*/
+
+//            String query = "select t from Team t";
+//            String query = "select t from Team t join fetch t.members";
+//            String query = "select distinct t from Team t join fetch t.members";
+            String query = "select t from Team t join t.members m";
+
+            List<Team> result = entityManager.createQuery(query, Team.class).getResultList();
+
+            System.out.println("result.size() = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
             }
 
             transaction.commit();
