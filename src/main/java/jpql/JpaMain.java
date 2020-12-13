d293dac034ca4c8f759984d54745fe5cb63e4119
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-13 23:19:47 +0900

    벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -32,29 +32,39 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             entityManager.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             entityManager.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             entityManager.persist(member3);
 
-            entityManager.flush();
+//            entityManager.flush();
+//            entityManager.clear();
+
+            //entityManager.flush(); 자동 호출
+            int resultCount = entityManager.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             entityManager.clear();
 
-            List<Member> resultList = entityManager.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            Member findMember = entityManager.find(Member.class, member1.getId());
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            System.out.println("resultCount = " + resultCount);
+
+            System.out.println("member1.getAge() = " + member1.getAge());
+            System.out.println("member2.getAge() = " + member2.getAge());
+            System.out.println("member3.getAge() = " + member3.getAge());
 
             transaction.commit();
         } catch (Exception e) {
